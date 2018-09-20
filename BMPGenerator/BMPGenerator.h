b57a219ae874b6be5cb0536bb8aa5097e1102fed
Andrei Chusov <lpsztemp@gmail.com>
2018-09-20 20:15:50 +1000

    Edited Makefile (issue 5); deleted Min, Max, MinTask, MaxTask and implemented extremum(2)Task (issue 4);

--- BMPGenerator/BMPGenerator.h
+++ BMPGenerator/BMPGenerator.h
@@ -32,6 +32,199 @@ typedef struct __tagRGBTRIPLE
 #define DWORDSIZE sizeof(std::uint32_t) //4
 #define WORDSIZE sizeof(std::uint16_t) //2
 
+//predicates
+template <class Compare, class T>
+struct impl_less_t
+{
+	constexpr bool operator()(const T& x, const T& y) const
+	{
+		return m_compare(x, y);
+	}
+	impl_less_t() = default;
+	explicit constexpr impl_less_t(const Compare& compare) :m_compare(compare) {}
+private:
+	Compare m_compare;
+};
+
+template <class Compare, class T>
+struct impl_equal_t
+{
+	constexpr bool operator()(const T& x, const T& y) const
+	{
+		return !m_compare(x, y) && !m_compare(y, x);
+	}
+	impl_equal_t() = default;
+	explicit constexpr impl_equal_t(const Compare& compare) :m_compare(compare) {}
+private:
+	Compare m_compare;
+};
+
+template <class Compare, class T>
+struct impl_less_equal_t
+{
+	constexpr bool operator()(const T& x, const T& y) const
+	{
+		return m_compare(x, y) || !m_compare(x, y) && !m_compare(y, x);
+	}
+	impl_less_equal_t() = default;
+	explicit constexpr impl_less_equal_t(const Compare& compare) :m_compare(compare) {}
+private:
+	Compare m_compare;
+};
+
+template <class Compare, class T>
+struct impl_greater_equal_t
+{
+	constexpr bool operator()(const T& x, const T& y) const
+	{
+		return !m_compare(x, y);
+	}
+	impl_greater_equal_t() = default;
+	explicit constexpr impl_greater_equal_t(const Compare& compare) :m_compare(compare) {}
+private:
+	Compare m_compare;
+};
+
+template <class Compare, class T>
+struct impl_greater_t
+{
+	constexpr bool operator()(const T& x, const T& y) const
+	{
+		return !m_compare(x, y) && (m_compare(x, y) || m_compare(y, x));
+	}
+	impl_greater_t() = default;
+	explicit constexpr impl_greater_t(const Compare& compare) :m_compare(compare) {}
+private:
+	Compare m_compare;
+};
+
+template <class Compare, class T>
+struct impl_not_equal_t
+{
+	constexpr bool operator()(const T& x, const T& y) const
+	{
+		return m_compare(x, y) || m_compare(y, x);
+	}
+	impl_not_equal_t() = default;
+	explicit constexpr impl_not_equal_t(const Compare& compare) :m_compare(compare) {}
+private:
+	Compare m_compare;
+};
+//////////
+
+template <class Iterator, class Compare>
+Iterator extremeTask(Iterator arrBegin, Iterator arrEnd, const Compare& compare)
+{
+	if (arrBegin == arrEnd)
+		return arrEnd;
+
+	using value_type = typename std::iterator_traits<Iterator>::value_type;
+	//constexpr auto PARALLEL_THRESHOLD = 2;
+	unsigned threadCount = std::thread::hardware_concurrency();
+	/*if (arrEnd - arrBegin < PARALLEL_THRESHOLD || threadCount < 2)
+		return std::less<double>(arrBegin, arrEnd);*/
+	std::vector<std::future<Iterator>> futures;
+	futures.reserve(threadCount);
+	int modulo = (arrEnd - arrBegin) % threadCount; 
+	auto i = arrBegin;
+
+	while (i < arrEnd)
+	{
+		std::int64_t part = (arrEnd - arrBegin) / threadCount; //count of elements in a thread
+		if (modulo != 0)
+			part += 1;
+		auto iEnd = i + part;
+
+		futures.emplace_back(std::async(std::launch::async, [](Iterator i, Iterator iEnd, Compare comp) -> Iterator
+		{
+			auto res = i;
+			for (auto it = i; it < iEnd; ++it)
+			{
+				if(comp(*it, *res))
+					res = it;
+			}
+			return res;
+		}, i, iEnd, compare));
+
+		if (modulo != 0)
+			modulo -= 1;
+		i += part;
+	}
+	auto it_extremum = futures[0].get();
+	for (std::size_t iFut = 1; iFut < futures.size(); ++iFut)
+	{
+		auto it_val = futures[iFut].get();
+		if (compare(*it_val, *it_extremum))
+			it_extremum = it_val;
+	}
+	return it_extremum;
+}
+
+template <class Iterator, class CompareLess, class CompareGreater>
+std::pair<Iterator, Iterator> extreme2Task(Iterator arrBegin, Iterator arrEnd, const CompareLess& less, const CompareGreater& greater)
+{
+		if (arrBegin == arrEnd)
+		return std::pair<Iterator, Iterator>(arrEnd, arrEnd);
+
+	using value_type = typename std::iterator_traits<Iterator>::value_type;
+	//constexpr auto PARALLEL_THRESHOLD = 2;
+	unsigned threadCount = std::thread::hardware_concurrency();
+	/*if (arrEnd - arrBegin < PARALLEL_THRESHOLD || threadCount < 2)
+		return std::less<double>(arrBegin, arrEnd);*/
+	std::vector<std::future<std::pair<Iterator, Iterator>>> futures;
+	futures.reserve(threadCount);
+	int modulo = (arrEnd - arrBegin) % threadCount;
+	auto i = arrBegin;
+
+	while (i < arrEnd)
+	{
+		std::int64_t part = (arrEnd - arrBegin) / threadCount;  //count of elements in a thread
+		if (modulo != 0)
+			part += 1;
+		auto iEnd = i + part;
+
+		futures.emplace_back(std::async(std::launch::async, [greater](Iterator i, Iterator iEnd, CompareLess compLess) -> std::pair<Iterator, Iterator>
+		{
+			std::pair<Iterator, Iterator> res = std::pair<Iterator, Iterator>(i, i);
+			for (auto it = i; it < iEnd; ++it)
+			{
+				if(compLess(*it, *res.first))
+					res.first = it;
+				if(greater(*it, *res.second))
+					res.second = it;
+			}
+			return res;
+		}, i, iEnd, less));
+
+		if (modulo != 0)
+			modulo -= 1;
+		i += part;
+	}
+	auto it_extremum = futures[0].get();
+	for (std::size_t iFut = 1; iFut < futures.size(); ++iFut)
+	{
+		auto it_val = futures[iFut].get();
+		if(less(*it_val.first, *it_extremum.first))
+			it_extremum.first = it_val.first;
+
+		if(greater(*it_val.second, *it_extremum.second))
+					it_extremum.second = it_val.second;
+	}
+	return it_extremum;
+}
+
+template <class Iterator, class CompareLess>
+auto extreme2Task(Iterator arrBegin, Iterator arrEnd, const CompareLess& less)
+{
+	return extreme2Task(arrBegin, arrEnd, less, impl_greater_t<CompareLess, double>(less));
+}
+
+template <class Iterator>
+auto extreme2Task(Iterator arrBegin, Iterator arrEnd)
+{
+	return extreme2Task(arrBegin, arrEnd, std::less<typename std::iterator_traits<Iterator>::value_type>());
+}
+
 inline unsigned scaleBetween(const double unscaledNum, const double min, const double max) 
 {
 	return unsigned(MAX_COLORS * (unscaledNum - min) / (max - min)); //scale from [min; max) to [0; MAX_COLORS)
@@ -56,7 +249,7 @@ bool generateBMP(const char* name, const double val_min, const double val_max, c
 		{
 			RGBTRIPLE rgb;
 			bool successCode = ValToRGB(GetValue(k, l), val_min, val_max, &rgb);
-			if(successCode) //but if successCode is false?
+			if(successCode)
 			{
 				fwrite(&rgb.rgbBlue, 1, 1, fp);
 				fwrite(&rgb.rgbGreen, 1, 1, fp);
@@ -68,100 +261,6 @@ bool generateBMP(const char* name, const double val_min, const double val_max, c
 	fclose(fp);
 	delete fp;
 	return true;
-}
-
-template <class Iterator>
-auto maxTask(Iterator arrBegin, Iterator arrEnd) 
-{
-	using value_type = typename std::iterator_traits<Iterator>::value_type;
-	constexpr auto PARALLEL_THRESHOLD = 10;
-	int threadCount = std::thread::hardware_concurrency();
-	if (arrEnd - arrBegin < PARALLEL_THRESHOLD || threadCount < 2)
-		return Max<Iterator>(arrBegin, arrEnd);
-	std::vector<std::future<value_type>> futures;
-	futures.reserve(threadCount);
-	int modulo = (arrEnd - arrBegin) % threadCount; //остаток
-	auto i = arrBegin;
-
-	while (i < arrEnd)
-	{
-		int part = (arrEnd - arrBegin) / threadCount; //сколько элементов в одном потоке
-		if (modulo != 0)
-			part += 1; //если остаток есть, то увеличиваем количество элементов в потоке на 1
-		auto iEnd = i + part;
-
-		futures.emplace_back(std::async(std::launch::async, Max<Iterator>, i, iEnd));
-
-		if (modulo != 0)
-			modulo -= 1;
-		i += part;
-	}
-
-	std::vector<value_type> results;
-	results.reserve(futures.size());
-	for (auto& fut : futures)
-		results.emplace_back(fut.get());
-	return maxTask(results.begin(), results.end());
-}
-
-template <class Iterator>
-auto minTask(Iterator arrBegin, Iterator arrEnd)
-{
-	using value_type = typename std::iterator_traits<Iterator>::value_type;
-	constexpr auto PARALLEL_THRESHOLD = 10;
-	int threadCount = std::thread::hardware_concurrency();
-	if (arrEnd - arrBegin < PARALLEL_THRESHOLD || threadCount < 2)
-		return Min<Iterator>(arrBegin, arrEnd);
-	std::vector<std::future<value_type>> futures;
-	futures.reserve(threadCount);
-	int modulo = (arrEnd - arrBegin) % threadCount; //остаток
-	auto i = arrBegin;
-
-	while (i < arrEnd)
-	{
-		int part = (arrEnd - arrBegin) / threadCount; //сколько элементов в одном потоке
-		if (modulo != 0)
-			part += 1; //если остаток есть, то увеличиваем количество элементов в потоке на 1
-		auto iEnd = i + part;
-
-		futures.emplace_back(std::async(std::launch::async, Min<Iterator>, i, iEnd));
-
-		if (modulo != 0)
-			modulo -= 1;
-		i += part;
-	}
-
-	std::vector<value_type> results;
-	results.reserve(futures.size());
-	for (auto& fut : futures)
-		results.emplace_back(fut.get());
-	return minTask(results.begin(), results.end());
-}
-
-template <class Iterator>
-auto Min(Iterator arrBegin, Iterator arrEnd)
-{
-	auto tempMin = *(arrBegin);
-
-	for (auto it = arrBegin; it < arrEnd; ++it)
-	{
-		if (*it < tempMin)
-			tempMin = *it;
-	}
-	return tempMin;
-}
-
-template <class Iterator>
-auto Max(Iterator arrBegin, Iterator arrEnd)
-{
-	auto tempMax = *(arrBegin);
-
-	for (auto it = arrBegin; it < arrEnd; ++it)
-	{
-		if (*it > tempMax)
-			tempMax = *it;
-	}
-	return tempMax;
 }
 
 template <class Callable>
@@ -184,16 +283,17 @@ bool generateBMP(const char* name, const Callable&& GetValue, const std::int32_t
 		}
 	}
 
-	auto minVal = minTask(heightmatrix, heightmatrix + arrSize);
-	auto maxVal = maxTask(heightmatrix, heightmatrix + arrSize);
+	auto minmaxVal = extreme2Task(heightmatrix, heightmatrix + arrSize, std::less<double>());
+	if (minmaxVal.first == heightmatrix + arrSize)
+		return false;
 
 	for (int l = 0; l < fHeight; ++l)
 	{
 		for (int k = 0; k < fWidth; ++k)
 		{
 			RGBTRIPLE rgb;
-			bool successCode = ValToRGB(GetValue(k, l), minVal, maxVal, &rgb);
-			if (successCode) //but if successCode is false?
+			bool successCode = ValToRGB(GetValue(k, l), *minmaxVal.first, *minmaxVal.second, &rgb);
+			if (successCode)
 			{
 				fwrite(&rgb.rgbBlue, 1, 1, fp);
 				fwrite(&rgb.rgbGreen, 1, 1, fp);
